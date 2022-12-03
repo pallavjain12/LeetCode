fe064b663590ec5d610716ec7ca6abb2033362f3
pallavjain12 <40729657+pallavjain12@users.noreply.github.com>
2022-12-03 23:51:21 +0530

    Time: 16 ms (6.02%), Space: 42.8 MB (5.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int reverse(int x) {
+        if (x < 999999999 && x >= 0) {
+            return reversePositive(x);
+        }
+        else if (x < 0 && x >= -999999999) {
+            return reverseNegative(x);
+        }
+        else {
+            return handleBigNumber(x);
+        }
+    }
+    public int reversePositive(int x) {
+        int res = 0;
+        while(x > 0) {
+            int r = x % 10;
+            res = res * 10 + r;
+            x /= 10;
+        }
+        return res;
+    }
+    private int reverseNegative(int x) {
+        x = x * -1;
+        x = reversePositive(x);
+        return -1 * x;
+    }
+    private int handleBigNumber(int x) {
+        System.out.println("Initial x : "+x);
+        if (x == -Integer.MIN_VALUE)    return 0;
+        boolean mul = (x < 0) ? true : false;
+        x = (x < 0) ? x * -1 : x;
+        int max = Integer.MAX_VALUE;
+        int duplicate = x;
+        int res = 0;
+        int divisor = 1000000000;
+        while(x > 0) {
+            int maxR = max / divisor;
+            int currR = x % 10;
+            System.out.println("maxR = " + maxR + " currR = " + currR);
+            System.out.println("res = " + res);
+            if (currR > maxR) {
+                return 0;
+            }
+            else if (currR < maxR) 
+                {res = reversePositive(duplicate);
+                break;}
+            else { 
+                x = x / 10; 
+                max %= divisor;
+                divisor /= 10;
+            }
+        }
+        if (mul) {
+            return res * -1;
+        }
+        else {
+            return res;
+        }
+    }
+}
