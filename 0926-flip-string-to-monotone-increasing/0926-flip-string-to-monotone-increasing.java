26c8d6262c3d23a7d6018f1b897459cff6b03a6c
pallavjain12 <40729657+pallavjain12@users.noreply.github.com>
2023-01-17 19:31:39 +0530

    Time: 11 ms (77.97%), Space: 42.6 MB (94.92%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -19,22 +19,3 @@ class Solution {
         return zeros;
     }
 }
-        // if(s.size()==0){
-        //     return 0;
-        // }
-        // int countFlips = 0;
-        // int countOnes = 0;
-        // for(auto ele:s){
-        //     if(ele=='0'){
-        //         if(countOnes==0)
-        //             continue;
-        //         else
-        //             countFlips++;
-        //     }else{
-        //         countOnes++;
-        //     }
-        //     if(countFlips>countOnes){
-        //         countFlips = countOnes;
-        //     }
-        // }
- 
