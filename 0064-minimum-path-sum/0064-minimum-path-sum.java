b7ed00dd6aaf8eaf446b4c55d2c340bab0c0d9c0
pallavjain12 <40729657+pallavjain12@users.noreply.github.com>
2023-04-01 13:24:50 +0530

    Time: 5 ms (5.83%), Space: 45.4 MB (36.52%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int[][] min = new int[grid.length][grid[0].length];
+        int rows = grid.length;
+        int columns = grid[0].length;
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0 && j != 0) {
+                    min[i][j] = min[i][j-1] + grid[i][j];
+                    continue;
+                }
+                else if (i == 0 && j == 0) {
+                    min[i][j] = grid[i][j];
+                    continue;
+                }
+                else if (i != 0 && j == 0) {
+                    min[i][j] = min[i-1][j] + grid[i][j];
+                    continue;
+                }
+                int min1 = (valid(i, j-1, rows, columns)) ? min[i][j-1] : 0;
+                int min2 = (valid(i-1, j, rows, columns)) ? min[i-1][j] : 0;
+                min[i][j] = Math.min(min1, min2) + grid[i][j];
+            }
+        }
+        // for (int[] i : min) {
+        //     for (int j : i) {
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        return min[rows-1][columns-1];
+    }
+    
+    private static boolean valid(int i, int j, int rows, int columns) {
+        if (i >= rows || i < 0 || j >= columns || j < 0)    return false;
+        return true;
+    }
+}
